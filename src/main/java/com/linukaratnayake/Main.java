91667ffbd48dbe1a4e5af2867048835504a9f872
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2025-01-27 15:51:30 +0530

    Create a separate package for client

--- src/main/java/com/linukaratnayake/Main.java
+++ src/main/java/com/linukaratnayake/Main.java
@@ -1,16 +1,12 @@
 package com.linukaratnayake;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.linukaratnayake.httpClient.Client;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.hc.client5.http.fluent.Request;
-import org.apache.hc.client5.http.fluent.Response;
 import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
-import org.apache.hc.core5.net.URIBuilder;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -22,7 +18,13 @@ public class Main {
         String toCur = "LKR";
         double amountUsd = 1.0;
 
-        String result = get("https://www.x-rates.com/calculator/", fromCur, toCur, amountUsd);
+        List<NameValuePair> params = new ArrayList<>();
+        // GET Query Parameters
+        params.add(new BasicNameValuePair("from", fromCur));
+        params.add(new BasicNameValuePair("to", toCur));
+        params.add(new BasicNameValuePair("amount", String.valueOf(amountUsd)));
+
+        String result = Client.get("https://www.x-rates.com/calculator/", params).toString();
 
         // Parse the HTML string
         Document doc = Jsoup.parse(result);
@@ -36,31 +38,5 @@ public class Main {
         } else {
             System.out.println("Value not found!");
         }
-    }
-
-    public static String get(String url, String fromCur, String toCur, double amount) {
-        String result = null;
-
-        List<NameValuePair> params = new ArrayList<>();
-        // GET Query Parameters
-        params.add(new BasicNameValuePair("from", fromCur));
-        params.add(new BasicNameValuePair("to", toCur));
-        params.add(new BasicNameValuePair("amount", String.valueOf(amount)));
-
-        // Add to the request URL
-        try {
-            URI uri = new URIBuilder(new URI(url))
-                    .addParameters(params)
-                    .build();
-            try {
-                Response response = Request.get(uri.toURL().toString()).execute();
-                result = response.returnContent().asString();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        return result;
     }
 }
---
+++ src/main/java/com/linukaratnayake/httpClient/Client.java
@@ -0,0 +1,40 @@
+package com.linukaratnayake.httpClient;
+
+import org.apache.hc.client5.http.fluent.Content;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Response;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static Content get(String url, List<NameValuePair> params) {
+        Content content = null;
+
+        // Add to the request URL
+        try {
+            URI uri = new URIBuilder(new URI(url))
+                    .addParameters(params)
+                    .build();
+            try {
+                Response response = Request.get(uri.toString()).execute();
+                content = response.returnContent();
+            } catch (IOException e) {
+                logger.error("An error occurred while processing the request", e);
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        return content;
+    }
+}
