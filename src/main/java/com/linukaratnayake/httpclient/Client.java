a07992071e70f7af650515cdb484ce6c95a22b03
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2025-02-20 17:19:04 +0530

    Modify get method to consume HttpEntity internally

--- src/main/java/com/linukaratnayake/httpclient/Client.java
+++ src/main/java/com/linukaratnayake/httpclient/Client.java
@@ -2,13 +2,13 @@ package com.linukaratnayake.httpclient;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.core5.http.HttpEntity;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Client {
 //    private static final Logger logger = LoggerFactory.getLogger(Client.class);
@@ -32,12 +32,17 @@ public class Client {
                 .build();
     }
 
-    public HttpEntity get(String url) throws IOException {
+    public InputStream get(String url) throws IOException {
         HttpGet getUrl = new HttpGet(url);
 
-        try (CloseableHttpResponse response = httpClient.execute(getUrl)) {
-            return response.getEntity();
-        }
+        return httpClient.execute(getUrl, response -> {
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                return entity.getContent();
+            } else {
+                return null;
+            }
+        });
     }
 
     public void close() throws IOException {
